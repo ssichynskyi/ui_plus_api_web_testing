0b5248fe74e91a4717a31a9ab7b8fd8bc3638ecd
Sergii Sichynskyi <s.sichynskyi@gmail.com>
2021-04-27 01:27:00 +0200

    add options for ui, api readonly and api write configs

--- framework/utilities/credentials_helper.py
+++ framework/utilities/credentials_helper.py
@@ -1,17 +1,29 @@
 import os
 
 
-class User:
+class UIUser:
     def __init__(self, login, password):
         self.login = login
         self.password = password
 
 
-normal_user = User(
-    login=os.getenv('STANDARD_USER_LOGIN'),
-    password=os.getenv('STANDARD_USER_PASSWORD')
+class APIUser:
+    def __init__(self, customer_key, customer_secret):
+        self.customer_key = customer_key
+        self.customer_secret = customer_secret
+
+
+normal_ui_user = UIUser(
+    login=os.environ['STANDARD_USER_LOGIN'],
+    password=os.environ['STANDARD_USER_PASSWORD']
 )
-locked_out_user = User(
-    login=os.getenv('LOCKED_OUT_USER_LOGIN'),
-    password=os.getenv('LOCKED_OUT_USER_PASSWORD')
+
+readonly_api_user = APIUser(
+    customer_key=os.environ['READONLY_API_CONSUMER_KEY'],
+    customer_secret=os.environ['READONLY_API_CONSUMER_SECRET']
+)
+
+read_write_api_user = APIUser(
+    customer_key=os.environ['READ_WRITE_API_CONSUMER_KEY'],
+    customer_secret=os.environ['READ_WRITE_API_CONSUMER_SECRET']
 )
