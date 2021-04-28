36da7e826b7979ef2aa3a4f22557cfe3c6ad1585
Sergii Sichynskyi <s.sichynskyi@gmail.com>
2021-04-28 22:54:45 +0200

    add more http requests and fix previously implemented

--- framework/api/api_caller.py
+++ framework/api/api_caller.py
@@ -1,13 +1,14 @@
 import requests
 from json import dumps
 from requests_oauthlib import OAuth1
+from typing import Dict
 from urllib.parse import urljoin
 
 from framework.utilities.credentials_helper import APIUser
 from framework.utilities.env_config import api_host_config
 
 
-DEFAULT_API_URL = api_host_config['url']
+DEFAULT_API_URL = api_host_config['URL']
 
 
 def get_user_auth(user: APIUser) -> OAuth1:
@@ -34,14 +35,38 @@ class APICaller:
             auth = get_user_auth(user)
         self._auth = auth
 
-    def get(self, extension, params):
-        """HTTP get request"""
+    def get(self, extension, params: dict = None, **kwargs):
+        """HTTP GET request"""
         url = urljoin(self._url, extension)
-        return requests.get(url, params, auth=self._auth)
+        return requests.get(url, params=params, auth=self._auth, **kwargs)
 
-    def post(self, extension: str, payload: dict, headers: dict = None):
-        """HTTP post request"""
+    def post(self, extension: str, payload: dict, headers: dict = None, **kwargs):
+        """HTTP POST request"""
         if not headers:
             headers = {'Content-Type': 'application/json'}
         url = urljoin(self._url, extension)
-        return requests.post(url, dumps(payload), headers=headers, auth=self._auth)
+        return requests.post(url, dumps(payload), headers=headers, auth=self._auth, **kwargs)
+
+    def put(self, extension: str, payload: dict, headers: dict = None, **kwargs):
+        """HTTP PUT request"""
+        if not headers:
+            headers = {'Content-Type': 'application/json'}
+        url = urljoin(self._url, extension)
+        return requests.put(url, dumps(payload), headers=headers, auth=self._auth, **kwargs)
+
+    def patch(self, extension: str, payload: dict, headers: dict = None, **kwargs):
+        """HTTP PATCH request"""
+        if not headers:
+            headers = {'Content-Type': 'application/json'}
+        url = urljoin(self._url, extension)
+        return requests.patch(url, dumps(payload), headers=headers, auth=self._auth, **kwargs)
+
+    def delete(self, extension: str, params: dict = None, **kwargs):
+        """HTTP DELETE request"""
+        url = urljoin(self._url, extension)
+        return requests.delete(url, auth=self._auth, params=params, **kwargs)
+
+    def options(self, extension: str, **kwargs):
+        """HTTP OPTIONS request"""
+        url = urljoin(self._url, extension)
+        return requests.options(url, auth=self._auth, **kwargs)
